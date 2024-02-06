98dbdf66774df9aaa1ffadad13a7cec27541959c
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2024-02-05 23:37:26 -0300

    add: linkedList.go

---
+++ algorithm/linkedList.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+type LinkedList struct {
+	Head *Node
+}
+
+type Node struct {
+	Data int
+	Next *Node
+}
+
+func (ll *LinkedList) Insert(n int) {
+	node := &Node{Data: n}
+	if ll.Head == nil {
+		ll.Head = node
+		return
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+	current.Next = node
+}
+
+func (ll *LinkedList) Delete(n int) {
+	if ll.Head == nil {
+		fmt.Println("Empty LL.")
+		return
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		if current.Next.Data == n {
+			current.Next = current.Next.Next
+			return
+		}
+		current = current.Next
+	}
+}
+
+func (ll *LinkedList) Display() {
+	current := ll.Head
+	for current != nil {
+		fmt.Println(current.Data)
+		current = current.Next
+	}
+}
+
+func main() {
+	ll := LinkedList{}
+	ll.Insert(1)
+	ll.Insert(2)
+	ll.Insert(3)
+
+	ll.Display()
+
+	ll.Delete(3)
+	ll.Display()
+}
