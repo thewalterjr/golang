4112dbb0ce708e9267a6f5570178ec1125237423
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2024-02-06 00:05:44 -0300

    wip

--- algorithm/linkedList.go
+++ algorithm/linkedList.go
@@ -31,6 +31,11 @@ func (ll *LinkedList) Delete(n int) {
 		return
 	}
 
+	if ll.Head.Data == n {
+		ll.Head = ll.Head.Next
+		return
+	}
+
 	current := ll.Head
 	for current.Next != nil {
 		if current.Next.Data == n {
@@ -39,6 +44,23 @@ func (ll *LinkedList) Delete(n int) {
 		}
 		current = current.Next
 	}
+}
+
+func (ll *LinkedList) Contain(n int) bool {
+	if ll.Head.Data == n {
+		ll.Head = ll.Head.Next
+		return true
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		if current.Next.Data == n {
+			return true
+		}
+		current = current.Next
+	}
+
+	return false
 }
 
 func (ll *LinkedList) Display() {
@@ -56,7 +78,10 @@ func main() {
 	ll.Insert(3)
 
 	ll.Display()
+	fmt.Println("ll.Contain(3)", ll.Contain(3))
+	fmt.Println("ll.Contain(5)", ll.Contain(5))
 
-	ll.Delete(3)
+	ll.Delete(1)
+	fmt.Println("ll.Contain(1)", ll.Contain(1))
 	ll.Display()
 }
