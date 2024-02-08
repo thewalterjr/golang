43e3b78eb57eb9d10e378b0eac577e3e8d92882b
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2024-02-08 14:35:16 -0300

    add: number_of_1_bits.go

--- leetcode/binary/number_of_1_bits.go
+++ leetcode/binary/number_of_1_bits.go
@@ -1,4 +1,16 @@
 func hammingWeight(num uint32) int {
+	count := 0
+	for num != 0 {
+        temp := (num & 1)
+        if temp == 1 {
+            count++
+        }
+        num = num >> 1
+	}
+	return count
+}
+
+func hammingWeight(num uint32) int {
 	aux := 0
 	for num != 0 {
 		aux += int(num & 1) 
