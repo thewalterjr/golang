6044d30e70211934e4684a866f87ee23c80daad2
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2024-02-16 00:15:21 -0300

    add: best-time-to-buy-and-sell-stock.go

---
+++ leetcode/Array/best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,16 @@
+func maxProfit(prices []int) int {
+    l,r := 0, 1 //left=buy righ=sell
+    maxP := 0
+
+    for r < len(prices) {
+        //profitable?
+        if prices[l] < prices[r] {
+            profit := prices[r] - prices[l]
+            maxP = max(maxP, profit)
+        } else {
+            l = r    
+        }
+        r += 1
+    }
+    return maxP
+}
